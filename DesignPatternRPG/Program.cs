e96d859045b536f247e903260437102084a079da
Sarunas Jacikas <fsaja018@edu.xamk.fi>
2023-11-27 11:44:21 +0200

    Add: Initialised project

---
+++ DesignPatternRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatternRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
