8fd33a2883352f436484d2b38de5ca957be2c428
Sarunas Jacikas <fsaja018@edu.xamk.fi>
2023-11-27 19:08:41 +0200

    Add: ItemDecorator

---
+++ DesignPatternRPG/ItemEnhancements/Item.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatternRPG.ItemEnhancements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//6.Decorator Pattern - Item Enhancements
+
+//    Task: Implement the Decorator pattern to add enhancements or abilities to items.
+//    Elements to Include:
+//        Base item class
+//        Decorators for item enhancements (e.g., fire damage, increased durability)
+namespace DesignPatternRPG.ItemEnhancements
+{
+    internal abstract class Item
+    {
+        public string name { get; protected set; }
+
+        protected Item(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ DesignPatternRPG/ItemEnhancements/ItemDecorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternRPG.ItemEnhancements
+{
+    // When using the decorator pattern, the decorator class must inherit from the base class
+    internal abstract class ItemDecorator : Item
+    {
+        // The decorator class has an instance of the base class
+        protected Item item;
+
+        // The decorator class must have a constructor that takes in an instance of the base class
+        protected ItemDecorator(Item item, string name) : base(name)
+        {
+            this.item = item;
+        }
+    }
+}
