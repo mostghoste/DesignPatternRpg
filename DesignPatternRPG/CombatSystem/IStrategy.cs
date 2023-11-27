cad4e5d5928e3b4b60d73d9e50cf467c6bcab657
Sarunas Jacikas <fsaja018@edu.xamk.fi>
2023-11-27 18:38:22 +0200

    Add: AggresiveStrategy

--- DesignPatternRPG/CombatSystem/IStrategy.cs
+++ DesignPatternRPG/CombatSystem/IStrategy.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
+
+//5.Strategy Pattern - Combat System
+
+//    Task: Use the Strategy pattern for implementing different combat strategies.
+//    Elements to Include:
+//        Different combat strategies (e.g., aggressive, defensive, balanced)
+//        Mechanism to switch strategies during combat
 
 namespace DesignPatternRPG.CombatSystem
 {
